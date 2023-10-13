c7dd84abd9510b47375148943e7ded96f33442c4
Anurag Tripathi <86647391+svamplike@users.noreply.github.com>
2023-10-13 11:47:08 +0530

    Create Pagerank.py
    Personal use

---
+++ Pagerank.py
@@ -0,0 +1,8 @@
+## Numpy library for matrix and vector manipulation
+import numpy as np ## Link matrix
+L = np.array([[0,1/2,0,0], [1/3, 0, 0, 1/2], [1/3, 0, 0, 1/2], [1/3, 1/2, 1, 0]])## Rank Matrix
+r = np.array([1/4,1/4,1/4,1/4])print(“i A B C D”) ## Print the link vector
+## Looping over Link matrix And Rank vector.
+for i in range(30):
+ print(i, round(r[0],2), round(r[1], 2), round(r[2], 2), round(r[3],2))
+ r = L@r
